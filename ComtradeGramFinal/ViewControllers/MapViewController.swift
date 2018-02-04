d31183d442dd082cb80ef293bf149f9c611721a1
Miroslav Mirovic <miroslav.mirovic@gmail.com>
2018-02-04 13:10:20 +0100

    Dodat pod 'LocationPicker'

--- ComtradeGramFinal/ViewControllers/MapViewController.swift
+++ ComtradeGramFinal/ViewControllers/MapViewController.swift
@@ -8,20 +8,60 @@
 
 import UIKit
 import SwiftInstagram
+import LocationPicker
+import CoreLocation
 
 class MapViewController: UIViewController {
-
+    
     var post: InstagramMedia?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let locationPicker = LocationPickerViewController()
+        
+        // you can optionally set initial location
+                _ = CLLocation(latitude: 44.834561, longitude: 20.411077)
+//        let initialLocation = Location(name: "Comtrade-Code", location: location)
+//        locationPicker.location = initialLocation
+        
+        // button placed on right bottom corner
+        locationPicker.showCurrentLocationButton = true // default: true
+        
+        // default: navigation bar's `barTintColor` or `.whiteColor()`
+        locationPicker.currentLocationButtonBackground = .red
+        
+        // ignored if initial location is given, shows that location instead
+        locationPicker.showCurrentLocationInitially = true // default: true
+        
+        locationPicker.mapType = .standard // default: .Hybrid
+        
+        // for searching, see `MKLocalSearchRequest`'s `region` property
+        locationPicker.useCurrentLocationAsHint = true // default: false
+        
+        locationPicker.searchBarPlaceholder = "Search places" // default: "Search or enter an address"
+        
+        locationPicker.searchHistoryLabel = "Previously searched" // default: "Search History"
+        
+        // optional region distance to be used for creation region when user selects place from search results
+        locationPicker.resultRegionDistance = 500 // default: 600
+        
+        locationPicker.completion = { location in
+            // do some awesome stuff with location
+            
+        }
+        
+        navigationController?.pushViewController(locationPicker, animated: true)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 }
 
